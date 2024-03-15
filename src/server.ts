83e823eccbeea5d29efc0d5596f01233bfea9a15
Darlan Almeida <121138070+Darlan-Almeida@users.noreply.github.com>
2024-03-15 17:24:10 -0300

    Tratando erros de criação de contas

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import mongoose, { Model, Schema } from 'mongoose';
 
 mongoose.connect('mongodb+srv://dhouglasbn:B5MY7pSLioF8u2PE@cluster-grupo3.1bxpdp3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-grupo3');
+
 const app = express();
 app.use(express.json());
 
@@ -21,26 +22,22 @@ app.listen(3333, () => console.log("Server listening on port 3333!"));
 app.post('/user', async (request: Request, response: Response) => {
     const { fullName, gender, age, email, password } = request.body;
 
-    const user = new User({
-        fullName: fullName,
-        gender: gender,
-        age: age,
-        email: email,
-        password: password
-    });
+    try {
+        const user = new User({
+            fullName: fullName,
+            gender: gender,
+            age: age,
+            email: email,
+            password: password
+        });
 
-    user.save();
+        await user.save();
 
-    return response.status(200).json(user.toJSON);
-})
+        console.log("Usuário cadastrado:", user);
 
-
-
-app.get('/user', async (request: Request, response: Response) => {
-    // código errado
-    const { id } = request.body;
-
-    const user = new User.findById(id);
-
-    return response.status(200).json(user.toJSON);
+        return response.status(200).json(user.toJSON());
+    } catch (error) {
+        console.error("Erro ao cadastrar usuário:", error);
+        return response.status(500).json({ error: "Erro ao cadastrar usuário" });
+    }
 });
